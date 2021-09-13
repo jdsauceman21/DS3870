a59a8f0c8f131622cb29ba91bebf4ff1d39f3327
SecretSauce2488 <89327514+SecretSauce2488@users.noreply.github.com>
2021-09-13 11:47:39 -0500

    update

--- PigPin/js/lib/app.js
+++ PigPin/js/lib/app.js
@@ -1,37 +1,23 @@
-$.getJSON("https://www.swollenhippo.com/getEmployeesByAPIKey.php?APIKey=Mickey2021!", function(result){;
-    console.log(restult);
-    arrEmployees = result;
-    buildEmployeeCard()
-    $.each(result,function(i,person){
-        console.log(person.FirstName)
-        console.log(person.FirstName+ '' person.LastName);
-        $'#txtEmail').val
-    })
+$('#btnTest').click(function() {
+    const decTaxRate = .0925;
+    let decHours = $('#txtHours').val();
+    let decRate = $('#txtPayRate').val();
+    console.log(decHours * decRate);
+});
+$('#cboEmployeeType').change(function() {
+    if($('#cboEmployeeType').val() == 'FULL'){
+        //$('#divHours').slideUp();
+        //$('#divHours').css('display','none');
+        $('#divHours').addClass('d-none');
+    }else {
+        $('#divHours').removeClass('d-none').slideDown();
+    }
+})
 
-
-
-
-
-    $('#btnTest').click(function() {
-        const decTaxRate = .0925;
-        let decHours = $('#txtHours').val();
-        let decRate = $('#txtPayRate').val();
-        console.log(decHours * decRate);
-    });
-    $('#cboEmployeeType').change(function() {
-        if($('#cboEmployeeType').val() == 'FULL'){
-            //$('#divHours').slideUp();
-            //$('#divHours').css('display','none');
-            $('#divHours').addClass('d-none');
-        }else {
-            $('#divHours').removeClass('d-none').slideDown();
-        }
-    })
-    
-    function onSignIn(googleUser) {
-        var profile = googleUser.getBasicProfile();
-        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-      }
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
