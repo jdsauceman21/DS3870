50fbe1da25232e12e88755ecbbcf4ff1d1fc8bdc
SecretSauce2488 <89327514+SecretSauce2488@users.noreply.github.com>
2021-09-14 21:06:00 -0500

    update

--- PigPin/UserInterfaceAssignment/js/lib/app.js
+++ PigPin/UserInterfaceAssignment/js/lib/app.js
@@ -1,4 +1,3 @@
-var arrEmployees;
 $.getJSON("https://www.swollenhippo.com/getEmployeesByAPIKey.php?APIKey=Mickey2021!", function(result){
     console.log(result);
     arrEmployees = result;
@@ -13,13 +12,13 @@ $.getJSON("https://www.swollenhippo.com/getEmployeesByAPIKey.php?APIKey=Mickey20
 function buildEmployeeCard(){
     $.each(arrEmployees,function(i,person){
         if(person.FirstName != 'John'){
-            let strHTML = '<div class="card col-6 offset-3 mt-5">';
+            let strHTML = '<div class="card col-3 mt-5">';
             strHTML += '<h3 class="text-center"><a href="mailto:' + person.Email + '">' + person.FirstName + ' ' + person.LastName + '</a></h3>';
             strHTML += '<h4 class="text-center">' + person.Postion +'</h4>';
             strHTML += '<h4 class="mt-3">Profile Details</h4>';
             strHTML += '<p>Hourly Rate: ' + person.HourlyRate + '</p>';
-            strHTML += '<p>Address:  586 Southern Lane, Cookeville, TN 38506</p>';
-            strHTML += '<p>Assignment:  Clement Hall</p>';
+            strHTML += '<p>Address:  123 South Willow ave, Cookeville, TN 38506</p>';
+            strHTML += '<p>Assignment:  Johnson Hall</p>';
             strHTML += '<div class="form-group">';
             strHTML += '<label for="txtPayRate">Pay Rate</label>';
             strHTML += '<input id="txtPayRate">';
@@ -39,8 +38,6 @@ $('#btnTest').click(function() {
 });
 $('#cboEmployeeType').change(function() {
     if($('#cboEmployeeType').val() == 'FULL'){
-        //$('#divHours').slideUp();
-        //$('#divHours').css('display','none');
         $('#divHours').addClass('d-none');
     }else {
         $('#divHours').removeClass('d-none').slideDown();
@@ -49,8 +46,8 @@ $('#cboEmployeeType').change(function() {
 
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('ID: ' + profile.getId()); 
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    console.log('Email: ' + profile.getEmail()); 
   }
