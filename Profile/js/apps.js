3aee9d157350944c4511527fd958036c1ca2a308
SecretSauce2488 <89327514+SecretSauce2488@users.noreply.github.com>
2021-09-29 10:51:21 -0500

    update

--- Profile/js/apps.js
+++
@@ -1,8 +0,0 @@
-var arrProfileData
-$.getJSON({"https://www.swollenhippo.com/getStaffByAPIKey.php?APIKey=DuffManSays,Phrasing!&Codename=Duchess",function();
-    
-})
-
-function fillProfile(Employee) {
-
-}
