a5f0884e64904853e6fec3fa291d5d5d6fbb9df0
SecretSauce2488 <89327514+SecretSauce2488@users.noreply.github.com>
2021-10-04 10:42:02 -0500

    updated class

--- jdsauceman21/ISISGetEmployee/ISISGetEmployee/getEmployee.cs
+++ jdsauceman21/ISISGetEmployee/ISISGetEmployee/getEmployee.cs
@@ -6,23 +6,36 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace ISISGetEmployee
 {
-    
+
     public static class getEmployee
     {
+        private class Agency
+        {
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public string Phone { get; set; }
+            public Agency(string strAgencyName, string strAddress, string strPhone)
+            {
+                Name = strAgencyName;
+                Address = strAddress;
+                Phone = strPhone;
+            }
+        }
         private class Employee
         {
             public string FirstName { get; set; }
             public string LastName { get; set; }
             public string CodeName { get; set; }
             public string Position { get; set; }
-            public string Status { get;}
-            public double WeeklyPay { get; }
-            public string Agency { get; set; }
-            public Employee(string strFirstName, string strLastName, string strCodeName, string strPosition, string strStatus, double dblPayRate, double dblHours, string strAgency)
+            public string Status { get; set; }
+            public double WeeklyPay { get; set; }
+            public Agency Agency { get; set; }
+            public Employee(string strFirstName, string strLastName, string strCodeName, string strPosition, string strStatus, double dblPayRate, double dblHours, string strAgency, Agency agSpyAgency)
             {
                 FirstName = strFirstName;
                 LastName = strLastName;
@@ -30,7 +43,7 @@ namespace ISISGetEmployee
                 Position = strPosition;
                 Status = strStatus;
                 WeeklyPay = dblPayRate * dblHours;
-                Agency = strAgency;
+                Agency = agSpyAgency;
             }
         }
 
@@ -40,39 +53,52 @@ namespace ISISGetEmployee
             ILogger log)
         {
             string strCodeName = req.Query["CodeName"];
-            string strAgency = req Query["Agency"];
+            string strAgency = req.Query["Agency"];
             log.LogInformation("HTTP trigger on getEmployee processed a request for: " + strCodeName);
 
-            
+            Agency ISIS = new Agency("ISIS", "10 E Broad St", "(931) 526-2125");
+            Agency CIA = new Agency("CIA", "10 E Broad St", "(931) 526-2125");
 
-            Employee Archer = new Employee("Sterling", "Archer", "Duchess", "Field Agent", "Active",23.75,18.50"ISIS");
-            Employee Lana = new Employee("Lana", "Kane", "Truckasaurus", "Field Agent", "Active",21.50,23.50"ISIS");
-            Employee Pam = new Employee("Pam", "Poovey", "Truckasaurus", "Field Agent", "Active",49.00,12"ISIS");
-            Employee Berry = new Employee("Berry", "", "Truckasaurus", "Field Agent", "Active", 23.75, 18.50"CIA");
-
+            Employee Archer = new Employee("Sterling", "Archer", "Duchess", "Field Agent", "Active", 23.75, 18.50, ISIS);
+            Employee Lana = new Employee("Lana", "Kane", "Truckasaurus", "Field Agent", "Active", 21.50, 23.50, ISIS);
+            Employee Pam = new Employee("Pam", "Poovey", "Snowball", "Human Resource Director", "Active", 49.00, 12, ISIS);
+            Employee Barry = new Employee("Barry", "Cyborg", "Duchess", "Field Agent", "Active", 23.75, 18.50, CIA);
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            
-            if (strCodeName == null)
+
+            Employee[] arrEmployees = new Employee[] { Archer, Lana, Pam, Barry };
+
+            List<Employee> lstISIS = new List<Employee>();
+            List<Employee> lstCIA = new List<Employee>();
+            foreach (Employee empCurrent in arrEmployees)
             {
-                return new OkObjectResult("Employee Not Found");
-            } else
-            {
-                if(strCodeName == "Duchess")
+                if (empCurrent.Agency.Name == "CIA")
                 {
-                    return new OkObjectResult(Archer);
-                } else if (strCodeName == "Truckasaurus")
-                {
-                    return new OkObjectResult(Lana);
+                    lstCIA.Add(empCurrent);
                 } else
                 {
-                    return new OkObjectResult("Employee Not Found");
+                    lstISIS.Add(empCurrent);
                 }
-                
             }
-            
-           
+                List<Employee> lstFoundEmployees = new List<Employee>();
+            foreach (Employee empCurrent in arrEmployees)
+            {
+                if (strCodeName == empCurrent.CodeName)
+                {
+                    lstFoundEmployees.Add(empCurrent);
+                }
+            }
+            if (lstFoundEmployees.Count > 0)
+            {
+                return new OkObjectResult(lstFoundEmployees);
+            }
+            else
+            {
+                return new OkObjectResult("Employee Not Found");
+            }
+
+
         }
     }
 }
